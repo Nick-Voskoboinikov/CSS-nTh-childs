0f05781220cc61041a1fb25c6e2600ca08351e99
Nick-Voskoboinikov <Nick@Voskoboinikov.com>
2023-08-23 03:25:49 +0200

    clippboard and accessibility improvements

---
+++ src/index.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded',() => {
+    const selectors =  document.querySelectorAll('td.selector');
+    selectors.forEach((selector) => {
+        selector.setAttribute('title', 'Click to copy');
+        selector.addEventListener('click', (event) => {
+            event.stopPropagation();
+            let selectorTarget = (event.target);
+            let thevalue = selectorTarget.innerText;
+            let tooltip = document.querySelector('.tooltiptext');
+            selectorTarget.style.backgroundColor='peachpuff';
+            selectorTarget.style.color='#0f0f0f';
+            navigator.clipboard.writeText(thevalue);
+            tooltip.innerHTML = "Copied - " + thevalue;
+            tooltip.style.opacity=1;
+            setTimeout(function (){
+                selectorTarget.style.cssText += 'transition: background-color 2s;';
+                selectorTarget.style.backgroundColor='';
+                selectorTarget.style.color='#728692';
+                tooltip.style.opacity=0;
+                setTimeout(function (){
+                    selectorTarget.style.removeProperty('transition');
+                }, 2500);
+            }, 3000);
+    
+      });
+    });
+    });
